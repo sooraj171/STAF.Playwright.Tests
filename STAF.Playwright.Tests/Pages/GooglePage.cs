bec364455b82110d1827531e43cb67efbd09448e
Sooraj <sooraj171@outlook.com>
2025-09-12 18:09:19 -0400

    Add project files.

---
+++ STAF.Playwright.Tests/Pages/GooglePage.cs
@@ -0,0 +1,45 @@
+﻿
+using Microsoft.Playwright;
+using STAF.Playwright.Framework;
+
+namespace STAF.Playwright.Pages
+{
+    public class GooglePage : BasePage
+    {
+        public IPage _google;
+        TestContext TestContext { get; set; }
+        string TestName { get; set; }
+        public GooglePage(IPage page, TestContext testContext) : base(page, testContext)
+        {
+            _google = page;
+            TestContext = testContext;
+            TestName = testContext.TestName??"NoName";
+        }
+
+        #region elements
+        public ILocator SearchBox => _google.Locator("[title='Search']"); //FindAppElementAsync("input[name='q']");
+
+        #endregion
+
+        public async Task VerifyGooglePageIsDisplayed()
+        {
+            if (await WaitForElementVisibleAsync(SearchBox, 5000))
+            {
+                
+                await HtmlResult.TC_ResultCreation(_google, TestName, "Google Page is displayed", "Google Page is displayed", "Pass", "");
+            }
+            else
+            {
+                await HtmlResult.TC_ResultCreation(_google, TestName, "Google Page is displayed", "", "Fail", "");
+                throw new Exception("");
+            }
+        }
+
+        public async Task SearchFor(string search)
+        {
+            var searchBox = SearchBox;
+            await EnterTextAsync(searchBox, search, TestName, "Search for " + search);
+            await searchBox.PressAsync("Enter");
+        }
+    }
+}
---
+++ STAF.Playwright.Tests/Tests/Test1.cs
@@ -0,0 +1,21 @@
+﻿using STAF.Playwright.Framework;
+using STAF.Playwright.Pages;
+
+namespace STAF.Playwright
+{
+    [TestClass]
+    public sealed class Test1 : BaseTest
+    {
+        //TestContext TestContext { get; set; }
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            await Page.GotoAsync("https://www.google.com");
+
+            GooglePage googlePage = new (Page, TestContext);
+            await googlePage.VerifyGooglePageIsDisplayed();
+            await googlePage.SearchFor("Playwright");
+        }
+    }
+    
+}
